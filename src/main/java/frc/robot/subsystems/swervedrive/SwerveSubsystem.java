162564084887182a3673d03170137a651291bf90
joyadomari-chun24 <97013614+MisterKermit@users.noreply.github.com>
2025-03-06 17:31:56 -1000

    Offsets fixed, inversion fixed, needs to tune PID
    More PID Tuning Neccesary
    -Test field oriented (Switch to spi)

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -102,6 +102,7 @@ public class SwerveSubsystem extends SubsystemBase
     swerveDrive.setModuleEncoderAutoSynchronize(false,
                                                 1); // Enable if you want to resynchronize your absolute encoders and motor encoders periodically when they are not moving.
     swerveDrive.setAutoCenteringModules(true);
+    swerveDrive.setModuleEncoderAutoSynchronize(true, 0.1);
 
     // swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
     if (visionDriveTest)

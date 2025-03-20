ca85a75edfc273b3a488f93b85c3b7f734edc46b
Tahan Bapna <156039959+AyoItzMe55@users.noreply.github.com>
2025-03-19 22:30:02 -1000

    more auto fixes
    FollowPathAutoCommand.java should now run an auto instead of a path so it can be simplified.

--- src/main/java/frc/robot/commands/swervedrive/auto/FollowPathAutoCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/FollowPathAutoCommand.java
@@ -5,28 +5,14 @@ import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDrive;
 
-public class FollowPathAuto extends CommandBase {
+
+public class FollowAuto extends CommandBase {
   private final SwerveDrive swerveDrive;
-  private final PPSwerveControllerCommand swerveCommand;
+  private final Command autoCommand;
 
   public FollowPathAuto(SwerveDrive swerveDrive) {
     this.swerveDrive = swerveDrive;
-    // load "SamplePath" from pathPlanner
-    // default maximum velocity and acceleration --> 3.0 m/s and 3.0 m/s2, change as needed
-    PathPlannerTrajectory trajectory = PathPlanner.loadPath("SamplePath", 3.0, 3.0);
-
-    // PPSwerveControllerCommand (command class in Pathplanner library) that will follow the trajectory 
-    swerveCommand = new PPSwerveControllerCommand(
-         trajectory,
-        swerveDrive::getPose,             // robot pose
-        swerveDrive.getKinematics(),      // swerve drive kinematics
-        swerveDrive.getXController(),     // PID controller for X 
-        swerveDrive.getYController(),     // PID controller for Y 
-        swerveDrive.getThetaController(), // PID controller for rotation
-        swerveDrive::setModuleStates,     // takes trajectory or path-following controller ex. wheel speeds and angles, and applies to swerve drive system
-        swerveDrive                       // drive subsystem
-    );
-    // ensure above code is correct, I made it look nice :)
+    autoCommand = new PathPlannerAuto("center_preload_blue_auto");
     addRequirements(swerveDrive);
   }
 

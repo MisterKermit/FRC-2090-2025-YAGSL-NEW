933125377ddd4856e2056ea80db5bb29811cd28b
Tahan Bapna <156039959+AyoItzMe55@users.noreply.github.com>
2025-03-20 15:42:52 -1000

    followpathautocommand.java fix
    should clear up errors. errors will still occur until scoringL3 state is added so that the namedcommand in robotcontainer works

--- src/main/java/frc/robot/commands/swervedrive/auto/FollowPathAutoCommand.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/FollowPathAutoCommand.java
@@ -5,34 +5,36 @@ import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDrive;
 
+// YAGSL drive system instea of swerve drive
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class FollowAuto extends CommandBase {
-  private final SwerveDrive swerveDrive;
+  private final SwerveSubsystem swerveSubsystem;
   private final Command autoCommand;
 
   public FollowPathAuto(SwerveDrive swerveDrive) {
-    this.swerveDrive = swerveDrive;
+    this.swerveSubsystem = swerveSubsystem;
     autoCommand = new PathPlannerAuto("center_preload_blue_auto");
-    addRequirements(swerveDrive);
+    addRequirements(swerveSubsystem);
   }
 
   @Override
   public void initialize() {
-    swerveCommand.initialize();
+    autoCommand.initialize();
   }
 
   @Override
   public void execute() {
-    swerveCommand.execute();
+    autoCommand.execute();
   }
 
   @Override
   public void end(boolean interrupted) {
-    swerveCommand.end(interrupted);
+    autoCommand.end(interrupted);
   }
 
   @Override
   public boolean isFinished() {
-    return swerveCommand.isFinished();
+    return autoCommand.isFinished();
   }
 }
